26060ca5fa4bda80e78a7aacca1546dadfc2fe4b
Luka Dubravcic <Luka@Teas-Mac-mini.local>
2017-11-07 12:51:35 +0100

    Set username endpoint

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -63,14 +63,12 @@ router.post('/register', (req, res) => {
 
 router.get('/', (req, res) => {
 
-    const user = req.user;
-
-    if (user !== undefined) {
+    if (req.user) {
         User.findById(req.user._id, (err, user) => {
             if (err) return res.status(500).send('Error fetching user data.');
             if (!user) return res.status(400).send("No user found.");
             else {
-                // posalji user data (napravljen je refresh)
+                // posalji user data (napravljen je page refresh)
                 res.send({
                     _id: user._id,
                     email: user.email,
@@ -83,7 +81,36 @@ router.get('/', (req, res) => {
     }
 });
 
+
+router.post('/username', (req, res) => {
+
+    if (req.user && req.body.username) {
+        User.findById(req.user._id, (err, user) => {
+            if (err) return res.status(500).send('There was a problem finding the user.');
+            if (!user) return res.status(400).send('No user found');
+
+            if (user.username) return res.status(400).send('Cannot update user with existing username.');
+
+            else if (!user.username) {
+
+                user.username = req.body.username;
+                user.save((err, result) => {
+                    if (err) return res.status(500).send('Error on saving username.');
+                    return res.send({
+                        _id: user._id,
+                        email: user.email,
+                        username: user.username
+                    });
+                });
+            }
+        });
+    } else {
+        res.status(400).json('Unauthorized access.');
+    }
+});
+
 router.get('/:id', (req, res) => {
+
     User.findById(req.params.id, (err, user) => {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
@@ -92,6 +119,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+
     User.findByIdAndRemove(req.params.id, (err, user) => {
         if (err) return res.status(500).send("There was a problem finding the user.");
         res.status(200).send("User " + user.name + " was deleted");
@@ -99,6 +127,7 @@ router.delete('/:id', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+
     User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, user) => {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
